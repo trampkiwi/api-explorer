f15573b7a1232c31e0d44fd3b152cf42bdb9a522
trampkiwi <starwatcherkiwi@gmail.com>
2018-12-30 21:21:27 +0900

    added Rover model

--- drone/models.py
+++ drone/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+class Rover(models.Model):
+    latitude = models.DecimalField(max_digits=11, decimal_places=8)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8)
+
+    direction = models.FloatField() # 0 is due north, clockwise increment
+
+    def __str__(self):
+        latlong = '(' + str(self.latitude) + ',' + str(self.longitude) + ')'
+        direct = str(self.direction)
+        return '[' + latlong + ',' + direct + ']'
